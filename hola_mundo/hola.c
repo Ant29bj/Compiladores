d026711fd7f90d939a5e88dc218b3fc5cf3bfe48
Bojorquez Espinoza Jesus Antonio <bojorquez29an@gmail.com>
2022-09-03 07:46:23 -0600

    añadi la fecha a los comentarios

--- hola_mundo/hola.c
+++ hola_mundo/hola.c
@@ -1,7 +1,8 @@
   /* 		Author      : Bojorquez Espinoza Jesus Antonio
    *  		Grupo       : 302 
    *  		Nombre      : Hola mundo 
-   *  		Descripcion : Hola mundo*/
+   *  		Descripcion : Hola mundo
+   *  		Programado  : Sabado-3-Septiembre*/
 
 #include <stdio.h>
 int main(){
